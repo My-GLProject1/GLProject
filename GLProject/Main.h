3e982c950d72267c0e688fe64c748d49965e0917
John Janzen <joalja2002@msn.com>
2016-03-15 20:27:03 -0700

    Tutorial 1 Finished

--- GLProject/Main.h
+++ GLProject/Main.h
@@ -6,20 +6,5 @@ class Main
 public:
 	Main();
 	~Main();
-	GLvoid KillGLWindow(GLvoid);
-	int DrawGLScene(GLvoid);
-	int InitGL(GLvoid);
-	GLvoid ReSizeGLScene(GLsizei, GLsizei);
-	BOOL CreateGLWindow(char*, int, int, int, bool);
-
-private:
-	HGLRC hRC = NULL;
-	HDC hDC = NULL;
-	HWND hwnd = NULL;
-	HINSTANCE hInstance;
-
-	bool keys[256];
-	bool active = TRUE;
-	bool fullscreen = TRUE;
 };
 
